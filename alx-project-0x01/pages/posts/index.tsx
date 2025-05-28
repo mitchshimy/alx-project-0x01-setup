9d17d79aae14d6b6bdb2d13c1d871fd8a08f0dad
shimy <ochiengmishy2016@gmail.com>
2025-05-28 14:10:26 +0300

    commit

---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/layout/Header";
+
+import React from "react";
+
+const PostsPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Posts Page</h1>
+      {/* Your content here */}
+    </div>
+  );
+};
+
+export default PostsPage;
---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/layout/Header";
+
+import React from "react";
+
+const UsersPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Users Page</h1>
+      {/* Your content here */}
+    </div>
+  );
+};
+
+export default UsersPage;
